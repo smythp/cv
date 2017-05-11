75e298f1ed751e21689665b71be3e27ac76c8044
Patrick Smyth <patricksmyth01@gmail.com>
2017-05-10 23:08:52 -0400

    Automatic push

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 emacsclient --eval "(let ((file \"$(pwd)/cv.org\"))
     (with-current-buffer (find-file-noselect file)
-      (org-md-export-to-markdown)))"
+    (progn
+      (org-latex-export-to-pdf)
+      (org-md-export-to-markdown))))"
 cp cv.md README.md
-git add cv.md cv.org README.md
+git add cv.md cv.org cv.pdf README.md 
 git commit -m "Automatic push"
 git push
