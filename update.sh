3c37b00ce112cd42c808359f9f71566baaf6c867
Patrick Smyth <patricksmyth01@gmail.com>
2017-05-29 16:46:18 -0400

    Don't add PDF download to PDF version

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@
 emacsclient --eval "(let ((file \"$(pwd)/cv.org\"))
     (with-current-buffer (find-file-noselect file)
     (progn
-      (org-latex-export-to-pdf)
-      (org-md-export-to-markdown))))"
+        (org-latex-export-to-pdf)
+	(let ((org-export-with-drawers t))
+        (org-md-export-to-markdown)))))"
 cp cv.md README.md
 git add cv.md cv.org cv.tex cv.pdf README.md 
 git commit -m "Automatic push"
