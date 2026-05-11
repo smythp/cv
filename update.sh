39023a79ce6bad2f23831224cc38def40ea82f84
Patrick Smyth <patrick.smyth@chainguard.dev>
2026-05-11 15:10:23 -0400

    Use setext headers in org-md export for consistency with prior cv.md style

--- update.sh
+++ update.sh
@@ -3,7 +3,8 @@ emacsclient --eval "(let ((file \"$(pwd)/cv.org\"))
     (with-current-buffer (find-file-noselect file)
     (progn
         (org-latex-export-to-pdf)
-	(let ((org-export-with-drawers t))
+	(let ((org-export-with-drawers t)
+              (org-md-headline-style 'setext))
         (org-md-export-to-markdown)))))"
 cp cv.md README.md
 git add cv.md cv.org cv.tex cv.pdf README.md 
